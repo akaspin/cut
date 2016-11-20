4113ab17438c12fda860d0e7627b930cdb898693
Alexander Dorofeev <aka.spin@gmail.com>
2016-11-20 03:21:29 +0100

    cc.Use

--- base.go
+++ base.go
@@ -4,6 +4,7 @@ import (
 	"github.com/spf13/cobra"
 	"reflect"
 	"strings"
+	"fmt"
 )
 
 type Command interface {}
@@ -21,18 +22,22 @@ type Runnable interface {
 
 // Attach command. If command implements Binder it will be also evaluated
 func Attach(c Command, binders []Binder, cmds ...*cobra.Command) (cc *cobra.Command) {
+	println(fmt.Sprint(c))
+
 	cc = &cobra.Command{}
 	for _, binder := range binders {
 		binder.Bind(cc)
 	}
 	if c1, ok := c.(Binder); ok {
 		c1.Bind(cc)
-	} else {
+	}
+	if cc.Use == "" {
 		// TypeOf().Name() return empty string on pointer types
 		cmd := reflect.TypeOf(c).String()
 		chunks := strings.Split(cmd, ".")
 		cc.Use = strings.ToLower(strings.TrimPrefix(chunks[len(chunks)-1], "*"))
 	}
+
 	if c1, ok := c.(Runnable); ok {
 		cc.RunE = func(cc *cobra.Command, args []string) (err error) {
 			return c1.Run(args...)
